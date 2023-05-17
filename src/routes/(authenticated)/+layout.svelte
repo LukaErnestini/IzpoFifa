a340c751dd66a88c1f47c4b936b01a39e7521c17
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-17 23:17:05 +0200

    add (group)

---
+++ src/routes/(authenticated)/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+</script>
+
+<div class="navbar bg-base-300">
+	<div class="flex-1">
+		<span class="btn btn-ghost normal-case text-xl">daisyUI</span>
+	</div>
+	<div class="flex-none">
+		<div class="dropdown dropdown-end">
+			<button tabindex="0" class="btn btn-ghost rounded-btn">
+				<Icon width="24" icon="material-symbols:menu-rounded" />
+			</button>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-200 rounded-box w-32 mt-4">
+				<li><a href="/gameday">Gameday</a></li>
+				<li><a href="/stats">Stats</a></li>
+				<li><a href="/settings">Settings</a></li>
+				<li>
+					<form action="/?/signout" method="post">
+						<button class="text-warning">Log Out</button>
+					</form>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	import '../app.css';
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
-	import Icon from '@iconify/svelte';
 
 	export let data;
 
@@ -23,31 +22,5 @@
 <svelte:head>
 	<title>Izpo-Fifa</title>
 </svelte:head>
-
-{#if data.session?.user}
-	<div class="navbar bg-base-300">
-		<div class="flex-1">
-			<span class="btn btn-ghost normal-case text-xl">daisyUI</span>
-		</div>
-		<div class="flex-none">
-			<div class="dropdown dropdown-end">
-				<button tabindex="0" class="btn btn-ghost rounded-btn">
-					<Icon width="24" icon="material-symbols:menu-rounded" />
-				</button>
-				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<ul tabindex="0" class="menu dropdown-content p-2 shadow bg-base-200 rounded-box w-32 mt-4">
-					<li><a href="/gameday">Gameday</a></li>
-					<li><a href="/stats">Stats</a></li>
-					<li><a href="/settings">Settings</a></li>
-					<li>
-						<form action="/?/signout" method="post">
-							<button class="text-warning">Log Out</button>
-						</form>
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-{/if}
 
 <slot />
