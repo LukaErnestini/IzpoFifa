d85e7d62189b90a84d6bb85ceba10c3ae7249c74
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-23 09:50:27 +0200

    set up image upload

--- src/routes/(authenticated)/a/players/[playerId]/+page.server.ts
+++ src/routes/(authenticated)/a/players/[playerId]/+page.server.ts
@@ -15,30 +15,27 @@ export const load = (async ({ params }) => {
 }) satisfies PageServerLoad;
 
 export const actions = {
-	save: async ({ request, fetch }) => {
+	save: async ({ request, fetch, locals: { supabase } }) => {
 		const data = await request.formData();
 		const id = data.get('id') as string;
 		const file = data.get('image') as File | null;
+
 		try {
 			if (file?.size) {
 				const ext = file?.name.split('.').pop();
 				const filename = nanoid() + '.' + ext;
-				// const s3 = new S3({
-				// 	region: 'eu-central-1',
-				// 	credentials: { accessKeyId: AWS_AK, secretAccessKey: AWS_SK }
-				// });
-				// await s3
-				// 	.putObject({
-				// 		Bucket: BUCKET_NAME,
-				// 		Key: filename,
-				// 		ACL: 'public-read',
-				// 		ContentType: file.type,
-				// 		Body: Buffer.from(await file.arrayBuffer())
-				// 	})
-				// 	.promise();
 
-				// const imageUri = 'https://izpo-fifa.s3.eu-central-1.amazonaws.com/' + filename;
-				// await prisma.player.update({ where: { id: +id }, data: { imageUri } });
+				const { data, error } = await supabase.storage.from('avatars').upload(filename, file, {
+					cacheControl: '3600',
+					upsert: false
+				});
+				// console.log('Data: ', data);
+				// console.log('Error: ', error);
+
+				const imageUri = supabase.storage.from('avatars').getPublicUrl(filename).data.publicUrl;
+				// console.log('Image URI: ', imageUri);
+
+				await prisma.player.update({ where: { id: +id }, data: { imageUri } });
 			}
 		} catch (error) {
 			console.log(error);
