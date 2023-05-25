99bee2fac69c22c51485a8a2351ba8f3cc529af6
Luka Ernestini <luka.ernestini@gmail.com>
2023-05-25 11:00:41 +0200

    put db query back inside trycatch

--- src/routes/(authenticated)/a/game/+layout.server.ts
+++ src/routes/(authenticated)/a/game/+layout.server.ts
@@ -5,23 +5,22 @@ import { fail, redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = async ({ parent }) => {
 	const data = await parent();
-	if (!data.activeGameday) {
+	if (!data.activeGameday?.games.find((game) => !game.finished)) {
 		throw redirect(302, 'gameday');
 	}
 	const gamedayId = data.activeGameday.id;
 
-	// TODO is this okay outside trycatch? I moved it outside so I can throw redirect...
-	const activeGame = await prisma.game.findFirst({
-		where: { gamedayId, finished: false },
-		include: {
-			teamA: { include: { players: { include: { player: true } } } },
-			teamB: { include: { players: { include: { player: true } } } }
-		}
-	});
-
-	if (!activeGame) throw redirect(302, 'gameday');
-
 	try {
+		const activeGame = await prisma.game.findFirst({
+			where: { gamedayId, finished: false },
+			include: {
+				teamA: { include: { players: { include: { player: true } } } },
+				teamB: { include: { players: { include: { player: true } } } }
+			}
+		});
+
+		if (!activeGame) throw fail(404, { error: 'Game not found' });
+
 		const attempts = await prisma.attempt.findMany({
 			where: { gameId: activeGame.id },
 			orderBy: { time: 'desc' }
