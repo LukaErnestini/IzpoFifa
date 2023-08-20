59e8a370ff55f3cfb44685cccdb3826a0390ae44
Luka Ernestini <luka.ernestini@gmail.com>
2023-08-20 10:33:16 +0200

    update teams style

--- src/routes/(authenticated)/a/teams/+page.svelte
+++ src/routes/(authenticated)/a/teams/+page.svelte
@@ -5,16 +5,18 @@
 {#if !data.teams.length}
 	<p>No teams yet.</p>
 {/if}
-<div class="flex flex-wrap justify-start p-4 gap-4">
-	{#each data.teams as team, i}
-		<a
-			class="h-12 w-24 flex flex-col items-center rounded-2xl"
-			href="teams/{team.id}"
-			style="background-color: {team.color};"
-		>
-			<span class="btn btn-accent h-1 min-h-[1rem] mt-1">
-				{team.name}
-			</span>
-		</a>
-	{/each}
+<div class="flex justify-center w-full">
+	<div class="flex flex-wrap justify-center p-4 gap-4 max-w-lg">
+		{#each data.teams as team, i}
+			<a
+				class="h-12 w-32 flex flex-col items-center rounded"
+				href="teams/{team.id}"
+				style="background-color: {team.color};"
+			>
+				<span class="btn btn-accent h-1 min-h-[1rem] mt-1">
+					{team.name}
+				</span>
+			</a>
+		{/each}
+	</div>
 </div>
